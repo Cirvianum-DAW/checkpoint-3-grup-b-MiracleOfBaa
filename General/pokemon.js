273a8b6690178639d06da786932b8b67694287c1
Raul Soneira <raul.soneira@cirvianum.cat>
2024-04-30 16:17:59 +0200

    primer commit

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,1 +1,17 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
+// Primero tenemos que poner el endpoint que vamos a usar para trabajar que en nuestro caso es pokemon
+const BASE_URL = 'https://pokeapi.co/api/v2/';
+
+async function listPokemons(id) {
+        return fetch(`${BASE_URL}pokemon/${id}/`)
+        .then((res => res.json())
+        .then((pokemon) => ({
+            id: pokemon.id,
+            name: pokemon.name,
+            height: pokemon.height,
+            weight: pokemon.weight,
+            abilities: pokemon.abilities,
+            sprites: pokemon.sprites,
+        }))
+        .catch((error) => console.error(error))
+    }
